96cdd99399714944e89675441a3a5d26e0f763cd
maximiliano.levi <maximiliano.levi@avature.net>
2018-06-22 15:55:15 -0300

    fix syntax and work on parsing

--- ApiFacade/FacadeStringBuilder.cs
+++ ApiFacade/FacadeStringBuilder.cs
@@ -3,8 +3,32 @@ using System.Text;
 
 namespace ApiFacade 
 {
-    public class FacadeStringBuilder : StringBuilder 
+    public class FacadeStringBuilder
     {
+        private readonly StringBuilder _builder;
+        private readonly StringBuilder _indentationBuilder;
+
+        public void Append(string Text)
+        {
+            _builder.Append($"{this.CreateIndentationString()}{Text}");
+        }
+
+        public void AppendLine(string Text)
+        {
+            this.Append($"{Text}{Environment.NewLine}");
+        }
+
+        private string CreateIndentationString()
+        {
+            _indentationBuilder.Clear();
+            for (var i = 0; i < IndentationLevel * 4; i++)
+            {
+                _indentationBuilder.Append(" ");
+            }
+            
+            return _indentationBuilder.ToString();
+        }
         
+        public int IndentationLevel { get; set; }
     }
 }
---
+++ ApiFacade/SourceParser.cs
@@ -0,0 +1,26 @@
+﻿using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Reflection;
+
+namespace ApiFacade
+{
+    public class SourceParser
+    {
+        public SourceParser()
+        {
+            
+        }
+
+        public void Parse()
+        {
+            var provider = CodeDomProvider.CreateProvider();
+            var result = provider.CompileAssemblyFromSource(CompilerParameters);
+            var assembly = result.CompiledAssembly;
+            var names = (from type in assembly.GetTypes()
+                from method in type.GetMethods(
+                    BindingFlags.Public | BindingFlags.NonPublic |
+                    BindingFlags.Instance | BindingFlags.Static)
+                select type.FullName + ":" + method.Name).Distinct().ToList();
+        }
+    }
+}
